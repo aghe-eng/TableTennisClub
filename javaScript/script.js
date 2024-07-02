ff909943ca100f3273f954ee654218bcf4f5e3fc
ehiaghe aisiri <ehiaghe.aisiri@gmail.com>
2024-07-02 11:41:58 +0200

    added ajax

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,10 +1,11 @@
 "use strict";
+
 $(document).ready(function() {
-    
+
 let slideIndex = 1;
 let slideTimeout;
 
-showSlides(slideIndex); 
+showSlides(slideIndex);
 
 function showSlides(n) {
     clearTimeout(slideTimeout);
@@ -12,11 +13,11 @@ function showSlides(n) {
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    
+
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     slides[slideIndex - 1].style.display = "block";
-    slideTimeout = setTimeout(function() { showSlides(slideIndex += 1); }, 7000); 
+    slideTimeout = setTimeout(function() { showSlides(slideIndex += 1); }, 7000);
 }
 
 function plusSlides(n) {
@@ -27,7 +28,7 @@ const showModal = document.querySelector(".show-modal");
 
 showModal.addEventListener('click', function () {
     const openModal = document.querySelector(".modal");
-    openModal.classList.remove("hidden");   
+    openModal.classList.remove("hidden");
 });
 const closeModal = document.querySelector(".close-modal");
 closeModal.addEventListener('click', function () {
